e5643118cedfb32697780931f0a70fdbe25ae7e9
sarebro <sarebro77@gmail.com>
2021-09-21 16:49:26 -0500

    finished

--- students.js
+++ students.js
@@ -1,14 +1,14 @@
 const students = [
-  { name: "Niko", color: "cyan" },
-  { name: "Bruno", color: "blue" },
-  { name: "Michael P", color: "salmon pink" },
-  { name: "Dario", color: "red" },
-  { name: "Seth", color: "blue" },
-  { name: "Todd", color: "purple" },
-  { name: "Praveena", color: "yellow" },
-  { name: "Josh", color: "navy" },
-  { name: "Dragan", color: "pink" },
-  { name: "Michael", color: "forest green" },
+	{ name: 'Niko', color: 'cyan' },
+	{ name: 'Bruno', color: 'blue' },
+	{ name: 'Michael P', color: 'salmon' },
+	{ name: 'Dario', color: 'red' },
+	{ name: 'Seth', color: 'blue' },
+	{ name: 'Todd', color: 'purple' },
+	{ name: 'Praveena', color: 'yellow' },
+	{ name: 'Josh', color: 'navy' },
+	{ name: 'Dragan', color: 'pink' },
+	{ name: 'Michael', color: 'green' }
 ];
 
 module.exports = students;
