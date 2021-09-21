512de26bb28f7fc49c9942de71f862cfa107e5e2
Michael Nazareth <mcnazareth@gmail.com>
2021-09-21 17:01:47 -0400

    michaels[i]

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const chalk = require('chalk') 
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 
 const app = express();
+const students = require("./students");
+console.log(students);
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-// app.get('/favoriteHoliday', (req, res) => {
-//     res.json([{ name: 'Haloween' }, { name: "Christams" }])
-// })
+app.get("/api", (req, res) => {
+  res.json(students);
+});
 
 // app.get('/cat', (request, response, next) => {
 //     response.sendFile(__dirname + '/views/cat-page.html');
 // });
-
-app.get('/home', (request, response, next) => {
-    response.sendFile(__dirname + '/views/home.html');
+app.get("/Home", (request, response, next) => {
+  response.sendFile(__dirname + "/views/Home.html");
+});
+app.get("/Contact", (request, response, next) => {
+  response.sendFile(__dirname + "/views/Contact.html");
+});
+app.get("/Works", (request, response, next) => {
+  response.sendFile(__dirname + "/views/Works.html");
 });
 
+app.get("/About", (request, response, next) => {
+  response.sendFile(__dirname + "/views/About.html");
+});
 
-app.listen(process.env.PORT || 5000, () => console.log('My first app listening on port 5000! '));
+app.get("/API", (request, response, next) => {
+  response.sendFile(__dirname + "/views/About.html");
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("My first app listening on port 5000! ")
+);
